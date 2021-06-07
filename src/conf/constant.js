1c8824a70ee9cc0934c2b80f0c9de787b3d9e0b1
OriX <1098888910@qq.com>
2021-06-07 11:20:16 +0800

    🔧 config:修改开发期间jwt的留存时长

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -3,14 +3,21 @@
  * @Author: OriX
  * @LastEditors: OriX
  */
+const { isProd } = require('../utils/env');
+let token_life = 60 * 60 * 1;
+let ref_token_life = 60 * 60 * 1 * 2;
+if (isProd) {
+  token_life = 60 * 60 * 24; // 1d
+  ref_token_life = 60 * 60 * 24 * 2; //2d
+}
 module.exports = {
   INIT_ADMIN_SECRET_KEY: 'msb@B5X_ori',
   CRYPTO_SECRET_KEY: '0i5o08oo5',
   JWT_CONFIG: {
     JWT_SECRET_KEY: 'GOX5`s&t8Fg',
     JWT_REFRESH_SECRET_KEY: 'Qr{{(ZL3',
-    JWT_TOKEN_LIFE: 60 * 60 * 24, // 24h
-    JWT_REFRESH_TOKEN_LIFE: 60 * 60 * 24 * 2, //2day
+    JWT_TOKEN_LIFE: token_life,
+    JWT_REFRESH_TOKEN_LIFE: ref_token_life,
   },
   // 初始化超级管理员的配置项
   INIT_ADMIN_CONFIG: {
