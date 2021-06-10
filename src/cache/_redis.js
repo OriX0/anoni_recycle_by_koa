332bad9c4d8ed04dbc2ebfb3d743c195332670f0
OriX <1098888910@qq.com>
2021-06-10 08:56:52 +0800

    📦 feat:增加redis 配置及cache操作

---
+++ src/cache/_redis.js
@@ -0,0 +1,57 @@
+/*
+ * @Description:  redis 操作
+ * @Author: OriX
+ * @LastEditors: OriX
+ */
+const redis = require('redis');
+const { REDIS_CONF } = require('../conf/db');
+
+// 创建客户端
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+// 检测redis的连接状态 遇到错误报错
+redisClient.on('error', err => {
+  console.error('redis error', err);
+});
+/**redis set方法
+ *
+ * @param {key}  键
+ * @param {value}  值
+ * @param {timeout}  缓存时间 单位s
+ */
+function set(key, value, timeout = 60 * 60) {
+  if (typeof value === 'object') {
+    value = JSON.stringify(value);
+  }
+  // 设置值
+  redisClient.set(key, value);
+  // 设置过期时间
+  redisClient.expire(key, timeout);
+}
+/**
+ * 获取值 返回一个promise
+ * @param {key} 键
+ * @returns
+ */
+function get(key) {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, (err, val) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (val == null) {
+        resolve(null);
+        return;
+      }
+      try {
+        resolve(JSON.parse(val));
+      } catch (error) {
+        resolve(val);
+      }
+    });
+  });
+}
+module.exports = {
+  set,
+  get,
+};
--- src/conf/db.js
+++ src/conf/db.js
@@ -2,7 +2,7 @@
  * @Description:数据库配置
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-06-03 17:21:04
+ * @LastEditTime: 2021-06-10 08:55:50
  */
 const { isProd } = require('../utils/env');
 let MYSQL_CONF = {
@@ -12,6 +12,10 @@ let MYSQL_CONF = {
   password: '12345wszcx',
   database: 'aoni_recycle',
 };
+let REDIS_CONF = {
+  host: '127.0.0.1',
+  port: 6379,
+};
 if (isProd) {
   MYSQL_CONF = {
     host: '生产环境的ip',
@@ -20,6 +24,10 @@ if (isProd) {
     password: '生产环境的密码',
     database: '生产环境的数据库名',
   };
+  REDIS_CONF = {
+    host: '生产环境的ip',
+    port: 6379,
+  };
 }
 
-module.exports = { MYSQL_CONF };
+module.exports = { MYSQL_CONF, REDIS_CONF };
