a9893e40aceb8fe0f1cabb6e6aaded504a08a528
OriX <1098888910@qq.com>
2021-06-10 15:50:34 +0800

    ✅ test:根据jwt的更改增加相关配置

--- test/admin/operationUser.test.js
+++ test/admin/operationUser.test.js
@@ -5,7 +5,7 @@
  */
 
 const server = require('../server');
-const { admin_jwt_token } = require('../testUserConf');
+const { admin_jwt_token, ref_jwt_token_in_cookie } = require('../testUserConf');
 const base_date = Date.now();
 const userName = `0${base_date}`;
 const password = `1${base_date}`;
@@ -16,8 +16,8 @@ test('新建一个用户 应该成功 ', async () => {
   const result = await server
     .post('/api/admin/users')
     .send({ userName, realName, city })
-    .set('Authorization', admin_jwt_token);
-
+    .set('Authorization', admin_jwt_token)
+    .set('cookie', ref_jwt_token_in_cookie);
   expect(result.body.errCode).toBe(0);
 });
 // 再次新建该用户 应该失败
@@ -25,13 +25,17 @@ test('再次新建该用户 应该失败 ', async () => {
   const result = await server
     .post('/api/admin/users')
     .send({ userName, realName, city })
-    .set('Authorization', admin_jwt_token);
+    .set('Authorization', admin_jwt_token)
+    .set('cookie', ref_jwt_token_in_cookie);
 
   expect(result.body.errCode).not.toBe(0);
 });
 // 重置这个用户的初始密码
 test('重置这个用户 应该重置成功 ', async () => {
-  const result = await server.patch(`/api/admin/users/${userName}/resetPwd`).set('Authorization', admin_jwt_token);
+  const result = await server
+    .patch(`/api/admin/users/${userName}/resetPwd`)
+    .set('Authorization', admin_jwt_token)
+    .set('cookie', ref_jwt_token_in_cookie);
   expect(result.body.errCode).toBe(0);
 });
 
@@ -40,13 +44,17 @@ test('修改这个用户的锁定状态 应该重置成功 ', async () => {
   const result = await server
     .patch(`/api/admin/users/${userName}/lock`)
     .send({ newLock: 1 })
-    .set('Authorization', admin_jwt_token);
+    .set('Authorization', admin_jwt_token)
+    .set('cookie', ref_jwt_token_in_cookie);
 
   expect(result.body.errCode).toBe(0);
 });
 // 获得这个用户的详情
 test('获得这个用户的详情 ', async () => {
-  const result = await server.get(`/api/admin/users/${userName}`).set('Authorization', admin_jwt_token);
+  const result = await server
+    .get(`/api/admin/users/${userName}`)
+    .set('Authorization', admin_jwt_token)
+    .set('cookie', ref_jwt_token_in_cookie);
   expect(result.body.errCode).toBe(0);
   expect(result.body.data.is_locked).toBe('1');
 });
--- test/testUserConf.js
+++ test/testUserConf.js
@@ -2,11 +2,13 @@
  * @Description: 测试所用的用户信息
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-06-07 10:45:05
+ * @LastEditTime: 2021-06-10 15:42:18
  */
 module.exports = {
   admin_jwt_token:
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVJbmZvIjoiNjY2IiwiaWF0IjoxNjIyOTg1MjQ4LCJleHAiOjE2MjMwNzE2NDh9.LREJ_o73XEVtIAZp6xLiUAbpWia9bik-Q8Cadmmtr2c',
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVJbmZvIjoiNjY2IiwiaWF0IjoxNjIzMzEwMjU4LCJleHAiOjE2MjMzMTM4NTh9.4ou5VNCQuewJqGZM1d6RIqnUto8gplpv2_SYquOUm0k',
+  ref_jwt_token_in_cookie:
+    'ref_t=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVJbmZvIjoiNjY2IiwiaWF0IjoxNjIzMzA4OTA0LCJleHAiOjE2MjMzMTYxMDR9.KcPd8IC6US9MnfdHCaUqDwciMoEO-fNK2ruxqSwXzYg',
   INIT_ADMIN_SECRET_KEY: 'msb@B5X_ori',
   HAVE_INIT_ADMIN: true,
   TEST_USER_NAME: '01623032864214',
