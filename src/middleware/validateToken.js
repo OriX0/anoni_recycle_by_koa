e2d7c82984572b783f134707c1c9ed8352f78743
OriX <1098888910@qq.com>
2021-06-11 15:20:21 +0800

    📦 refactor:重构了验证中间件的判断顺序

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -4,7 +4,12 @@
  * @LastEditors: OriX
  */
 const { get } = require('../cache/_redis');
-const { authTokenOverdueInfo, authTokeRefUnableValidInfo, authTokenUnableValidInfo } = require('../model/ErrorInfo');
+const {
+  authTokenOverdueInfo,
+  authTokeRefUnableValidInfo,
+  authTokenUnableValidInfo,
+  authTokeAccUnableValidInfo,
+} = require('../model/ErrorInfo');
 const { ErrorModel } = require('../model/BaseModel');
 const { noTokenValidUrlList, justRefTokenValidUrlList } = require('../conf/constant');
 const { verify_Token } = require('../utils/jwt_token');
@@ -26,8 +31,16 @@ async function ValidateToken(ctx, next) {
   // 修改acc_token
   acc_token = acc_token.split(' ')[1];
   // 3.从redis中取 相关键 判断是否在黑名单中
-  const refInBlackList = await get(ref_token);
-  const accInBlackList = await get(acc_token);
+  let refInBlackList = null;
+  let accInBlackList = null;
+  try {
+    refInBlackList = await get(ref_token);
+    accInBlackList = await get(acc_token);
+  } catch (error) {
+    ctx.status = 401;
+    ctx.body = new ErrorModel(authTokenUnableValidInfo);
+    return;
+  }
   // 3.1 如果存在于黑名单 返回401 让重新登录
   if (refInBlackList && accInBlackList) {
     ctx.status = 401;
@@ -36,22 +49,23 @@ async function ValidateToken(ctx, next) {
   // 4.判断是否有效
   const ref_verify = verify_Token(ref_token, 2) == 1;
   const acc_verify = verify_Token(acc_token, 1) == 1;
-  // 4.1判断当前的url是否只用验证 ref_verify
+
+  //  如果验证ref_verify不通过 返回错误
+  if (!ref_verify) {
+    ctx.status = 401;
+    ctx.body = new ErrorModel(authTokeRefUnableValidInfo);
+    return;
+  }
+  // 4.1判断当前的url是否只用验证 如果运行到这一步 说明通过 直接进入下一个
   if (justRefTokenValidUrlList.includes(currentUrl)) {
-    //  如果验证不通过 返回错误
-    if (!ref_verify) {
-      ctx.status = 401;
-      ctx.body = new ErrorModel(authTokeRefUnableValidInfo);
-      return;
-    }
     // 验证通过 调用next
     await next();
     return;
   }
-  // 4.2 如果 验证不通过
-  if (!(ref_verify && acc_verify)) {
+  // 4.2 验证证acc
+  if (!acc_verify) {
     ctx.status = 401;
-    ctx.body = new ErrorModel(authTokenUnableValidInfo);
+    ctx.body = new ErrorModel(authTokeAccUnableValidInfo);
     return;
   }
   await next();
